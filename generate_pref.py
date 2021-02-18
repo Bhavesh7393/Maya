21fe275bebb3192bd98cec9c3a9bc73c38db8d71
Bhavesh7393 <78908736+Bhavesh7393@users.noreply.github.com>
2021-02-18 22:39:08 +0530

    Update generate_pref.py

--- generate_pref.py
+++ generate_pref.py
@@ -1,43 +1,51 @@
-# Generate Pref on selected objects on current frame
-# on the last line write True or False to create or delete Pref attribute
-# Just drag and select objects on viewport and run the script
+# Generate Pref on selected objects on current frame.
+# on the last line write True or False to create or delete Pref.
 
 import maya.cmds as cmds
 
 sel = cmds.ls(selection=True)
-shp = cmds.listRelatives(type='shape', fullPath=True)
+shp = cmds.listRelatives(sel, type='shape', allDescendents=True, noIntermediate=True, fullPath=True)
+trans = cmds.listRelatives(shp, type='transform', parent=True, fullPath=True)
 
 def create_pref(sel, shp):
     for obj in range(len(shp)):
-        if cmds.attributeQuery('mtoa_varying_Pref', node=shp[obj], exists=True) == False:
+        if cmds.attributeQuery('mtoa_varying_Pref', node=shp[obj], exists=True) == False and cmds.attributeQuery('Pref', node=shp[obj], exists=True) == False:
             cmds.addAttr(shp[obj], longName='mtoa_varying_Pref', dataType='vectorArray')
+            cmds.addAttr(shp[obj], longName='Pref', dataType='vectorArray')
             
-        cmds.select(sel[obj]+'.vtx[*]')
-        allvtx = cmds.ls(selection=True, flatten=True)
-        vtxpos = []
+            cmds.select(trans[obj]+'.vtx[*]')
+            allvtx = cmds.ls(selection=True, flatten=True)
+            vtxpos = []
+            
+            for vtx in allvtx:
+                ppos = cmds.pointPosition(vtx)
+                vtxpos.append(ppos)
+                
+            cmds.setAttr(shp[obj] + '.mtoa_varying_Pref', len(vtxpos), *vtxpos, type='vectorArray')
+            cmds.setAttr(shp[obj] + '.Pref', len(vtxpos), *vtxpos, type='vectorArray')
         
-        for vtx in allvtx:
-            ppos = cmds.pointPosition(vtx)
-            vtxpos.append(ppos)
-            
-        cmds.setAttr(shp[obj] + '.mtoa_varying_Pref', len(vtxpos), *vtxpos, type='vectorArray')
+        else:
+            pass
         
     cmds.select(sel)
     
 def del_pref(shp):
     for obj in shp:
-        if cmds.attributeQuery('mtoa_varying_Pref', node=obj, exists=True) == True:
+        if cmds.attributeQuery('mtoa_varying_Pref', node=obj, exists=True) == True and cmds.attributeQuery('Pref', node=obj, exists=True) == True:
             cmds.deleteAttr(obj+'.mtoa_varying_Pref')
+            cmds.deleteAttr(obj+'.Pref')
+        else:
+            pass
             
 def pref(create):
     if create == True:
         create_pref(sel, shp)
         print
-        print('Pref attribute is generated on frame %s' % cmds.currentTime( query=True ))
+        print('Pref is generated on frame %s' % cmds.currentTime( query=True ))
         
     else:
         del_pref(shp)
         print
-        print('Pref attribute is deleted')
+        print('Pref is deleted')
         
-pref(create=True)    #<== use True or False to create or delete Pref attribute
+pref(create=True)    # <== use True or False to create or delete Pref.
